b198c8a375892b329a646ebacb6f2e243cf270f1
yuanwenyu <2891679283@qq.com>
2021-09-22 17:30:20 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
  import _ from 'lodash';
-
+ import './style.css';
+ import Icon from './icon.png';
   function component() {
     var element = document.createElement('div');
 
    // Lodash, currently included via a script, is required for this line to work
    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    
+  // 将图像添加到我们现有的 div。
+  var myIcon = new Image();
+  myIcon.src = Icon;
 
+  element.appendChild(myIcon);
     return element;
   }
 
